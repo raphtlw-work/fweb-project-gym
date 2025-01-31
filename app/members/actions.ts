daff12f11fe008fac4384b9f8976c8a04b286451
Raphael Tang <raphpb1912@gmail.com>
2025-01-31 22:58:34 +0800

    refactor: Improve fetchMemberByMatriculation error handling and return type

--- app/members/actions.ts
+++ app/members/actions.ts
@@ -5,18 +5,24 @@ import { Member } from "@/lib/schema";
 import { ObjectId } from "mongodb";
 import { revalidatePath } from "next/cache";
 
-export async function fetchMemberByMatriculation(matriculation: string): Promise<Member | null> {
+export async function fetchMemberByMatriculation(
+  matriculation: string
+): Promise<Member | null> {
   try {
     const db = await connectToDatabase();
     const collection = db.collection("members");
 
-    const member = await collection.findOne({ matriculationNumber: matriculation });
-    return member ? (member as Member) : null;
+    const member = await collection.findOne({
+      matriculationNumber: matriculation,
+    });
+    return member
+      ? ({ id: member._id.toString(), ...member } as unknown as Member)
+      : null;
   } catch (error) {
-    console.error("Failed to fetch member by matriculation:", error);
-    return null;
+    throw new Error("Failed to fetch member by matriculation:");
   }
 }
+
 export async function updateMember(member: Member) {
   try {
     const db = await connectToDatabase();
@@ -39,5 +45,4 @@ export async function updateMember(member: Member) {
     console.error("Failed to update member:", error);
     throw new Error("Failed to update member");
   }
-}
 }
