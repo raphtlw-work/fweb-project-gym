a90619bbaea2da5a779db44f1c671df2bcb42d6e
Raphael Tang <raphpb1912@gmail.com>
2025-02-11 05:41:55 +0800

    feat: add authentication session

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -1,50 +1,71 @@
 "use server";
 
 import { connectToDatabase } from "@/lib/db";
+import { Admin, Member } from "@/lib/schema";
 import bcrypt from "bcrypt";
+import { createSession } from "./session";
+import { redirect } from "next/navigation";
 
 export async function loginMemberAction(formData: FormData) {
   const email = formData.get("email")?.toString() || "";
   const password = formData.get("password")?.toString() || "";
+
+  if (email.length === 0) {
+    throw new Error("No email");
+  }
+  if (password.length === 0) {
+    throw new Error("No password");
+  }
+
   const db = await connectToDatabase();
   const user = await db.collection("members").findOne({ email });
   if (!user) {
     throw new Error("Invalid credentials");
   }
+
   const isValid = await bcrypt.compare(password, user.passwordHash);
   if (!isValid) {
     throw new Error("Invalid credentials");
   }
+
   const { _id, ...rest } = user;
-  // TODO: create session or token
-  return {
-    success: true,
-    user: {
-      id: _id.toString(),
-      ...rest,
-    },
-  };
+  const returned = {
+    id: _id.toString(),
+    ...rest,
+  } as Member;
+
+  await createSession(returned.id);
+  redirect("/");
 }
 
 export async function loginAdminAction(formData: FormData) {
   const email = formData.get("email")?.toString() || "";
   const password = formData.get("password")?.toString() || "";
+
+  if (email.length === 0) {
+    throw new Error("No email");
+  }
+  if (password.length === 0) {
+    throw new Error("No password");
+  }
+
   const db = await connectToDatabase();
   const admin = await db.collection("admins").findOne({ email });
   if (!admin) {
     throw new Error("Invalid credentials");
   }
+
   const isValid = await bcrypt.compare(password, admin.passwordHash);
   if (!isValid) {
     throw new Error("Invalid credentials");
   }
+
   const { _id, ...rest } = admin;
-  // TODO: create session or token
-  return {
-    success: true,
-    admin: {
-      id: _id.toString(),
-      ...rest,
-    },
-  };
+  const returned = {
+    id: _id.toString(),
+    ...rest,
+  } as Admin;
+
+  await createSession(returned.id);
+  redirect("/");
 }
